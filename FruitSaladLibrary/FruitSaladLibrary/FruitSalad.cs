be026d34a0e3b49ca8dae17cd87674db8b08624b
Naggy2005 <156240986+Naggy2005@users.noreply.github.com>
2025-01-21 19:40:49 -0500

    Completed Fruit salad Example
    Example of OOP inheritance and abstraction.

---
+++ FruitSaladLibrary/FruitSaladLibrary/FruitSalad.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FruitSaladLibrary
+{
+    public class FruitSalad
+    {
+        public List<Fruit> fruits;
+        public int servings; 
+
+        // Constructor
+        public FruitSalad(List<Fruit> ingredients, int numServings)
+        {
+            fruits = new List<Fruit>(ingredients);
+            servings = numServings;
+        }
+        public override string ToString()
+        {
+            string fruitDetails = string.Join(", ", fruits);
+            return $" {servings} servings of fruit salad with [{fruitDetails}]";
+        }
+        //Add a fruit to the salad
+        public void Add(Fruit fruit)
+        {
+            fruits.Add(fruit);
+
+        }
+        public string Serve()
+        {
+            if (servings > 0)
+            {
+                servings--;
+                return "enjoy your fruit salad!";
+            }
+            else
+            {
+                return "Soory, no fruit salad for you";
+            }
+        }
+
+    }
+}
